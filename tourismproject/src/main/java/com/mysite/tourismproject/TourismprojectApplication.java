0fc6483cc9c87f9a5abbce2b6a9e356f8242c47e
Gun-Hee-SON <songunhee5426@gmail.com>
2024-07-11 17:06:23 +0900

    start

---
+++ tourismproject/src/main/java/com/mysite/tourismproject/TourismprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.tourismproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismprojectApplication.class, args);
+	}
+
+}
---
+++ tourismproject/src/test/java/com/mysite/tourismproject/TourismprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.tourismproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
