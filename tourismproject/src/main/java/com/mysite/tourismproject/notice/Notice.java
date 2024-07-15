22b08954073b1817d021389c540d7a95994f92fa
Hokkey16 <louis7016@naver.com>
2024-07-15 17:37:37 +0900

    notice(공지사항) 백엔드 구축 시작
    Notice.java, NoticeRepository.interface 생성

--- tourismproject/src/main/java/com/mysite/tourismproject/notice/Notice.java
+++ tourismproject/src/main/java/com/mysite/tourismproject/notice/Notice.java
@@ -1,5 +1,28 @@
 package com.mysite.tourismproject.notice;
 
-public class Notice {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Notice {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate; 
+    
 }
