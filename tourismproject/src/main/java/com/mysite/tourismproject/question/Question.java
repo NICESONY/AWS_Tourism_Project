b4dda2d9fdf6efdda1ce69f0758aa7c26a57cadd
nanakyeong <129843360+nanakyeong@users.noreply.github.com>
2024-07-16 09:23:48 +0900

    병합 후 오류 수정

--- tourismproject/src/main/java/com/mysite/tourismproject/question/Question.java
+++ tourismproject/src/main/java/com/mysite/tourismproject/question/Question.java
@@ -1,5 +1,29 @@
 package com.mysite.tourismproject.question;
 
-public class Question {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
 }
