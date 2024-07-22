2bbd862486122b29b44c9eea3e90581c60223675
rktndnjseh <park.castle3@gmail.com>
2024-07-22 13:19:48 +0900

    v

--- tourismproject/src/main/java/com/mysite/tourismproject/restaurant/RestaurantService.java
+++ tourismproject/src/main/java/com/mysite/tourismproject/restaurant/RestaurantService.java
@@ -1,17 +1,23 @@
 package com.mysite.tourismproject.restaurant;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mysite.tourismproject.picture.Picture;
+import com.mysite.tourismproject.picture.PictureService;
 
 @Service
 public class RestaurantService {
 	
 	 @Autowired
 	    private RestaurantRepository restaurantRepository;
-	    
+	 @Autowired
+	    private PictureService pictureService;
 	    public void locationcreate(Restaurant restaurant) {
 	        restaurantRepository.save(restaurant);
 	       
@@ -24,4 +30,21 @@ public class RestaurantService {
 		Optional<Restaurant>or = restaurantRepository.findById(id);
 		return or.get();
 	}
+	public Map<Integer, Picture> getFirstPicturesForRestaurants(List<Restaurant> restaurantList) {
+        Map<Integer, Picture> firstPictures = new HashMap<>();
+        for (Restaurant restaurant : restaurantList) {
+            List<Picture> pictures = pictureService.findPicturesByRestaurantId(restaurant.getId());
+            if (!pictures.isEmpty()) {
+                for (Picture pict : pictures) {
+                    if (pict != null) {
+                        firstPictures.put(restaurant.getId(), pict);
+                        break;
+                    }
+                }
+            } else {
+                firstPictures.put(restaurant.getId(), null); // 사진이 없는 경우 null 추가
+            }
+        }
+        return firstPictures;
+    }
 }
