5cb7c569b51fed970f09749bdc64afb701b0e75e
kimdonghwan <0517dhkim@gmail.com>
2024-07-23 15:44:59 +0900

    레이아웃 추가

--- tourismproject/src/main/java/com/mysite/tourismproject/hotel/Hotel.java
+++ tourismproject/src/main/java/com/mysite/tourismproject/hotel/Hotel.java
@@ -2,4 +2,16 @@ package com.mysite.tourismproject.hotel;
 
 public class Hotel {
 
+	private String name;
+	private String checkInDate;
+	private String checkOutDate;
+	private int numberOfPeople;
+
+	public Hotel(String name, String checkInDate, String checkOutDate, int numberOfPeople) {
+	        this.name = name;
+	        this.checkInDate = checkInDate;
+	        this.checkOutDate = checkOutDate;
+	        this.numberOfPeople = numberOfPeople;
+	    }
+	
 }
