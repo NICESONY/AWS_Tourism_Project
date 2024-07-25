e692cfd09f00e3734bde58bc06ecb903b769d96a
Gun-Hee-SON <songunhee5426@gmail.com>
2024-07-25 10:21:37 +0900

    Update layout.html

---
+++ tourismproject/src/main/java/com/mysite/tourismproject/AdminController.java
@@ -0,0 +1,16 @@
+/*
+ * package com.mysite.tourismproject;
+ * 
+ * 
+ * import org.springframework.stereotype.Controller; import
+ * org.springframework.web.bind.annotation.GetMapping;
+ * 
+ * 
+ * @Controller public class AdminController {
+ * 
+ * @GetMapping("/notice/add/**") public String adminNotice() { return
+ * "adminNotice"; }
+ * 
+ * @GetMapping("/accessDenied") public String accessDenied() { return
+ * "accessDenied"; } }
+ */
